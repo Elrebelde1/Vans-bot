36a459c71bd904fd15bf92b659b7b309926ebfa3
Wichi <geraldorincon52@gmail.com>
2025-11-03 20:24:14 -0400

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,8 +1,15 @@
-let handler = async (m, { conn, isAdmin, isROwner} ) => {
+Let handler = async (m, { conn, isAdmin, isROwner} ) => {
+    // Solo un administrador o el dueño del bot pueden 'encender las luces'
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+    
+    // Quita la prohibición del chat: ¡El bot vuelve del Polo Norte!
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '✅ Bot activo en este grupo.', m, rcanal)
-    await m.react('✅')
+    
+    // Mensaje festivo de confirmación
+    await conn.reply(m.chat, '🌟 ¡Luces de Navidad Encendidas! El Bot está *activo* de nuevo en este grupo. ¡Felices Fiestas!', m, rcanal)
+    
+    // Reacción festiva
+    await m.react('🎄')
 }
 handler.help = ['desbanearbot']
 handler.tags = ['group']
