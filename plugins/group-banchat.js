32e02b02acf4681d5ec7f30cdbe740d2c1d740aa
Wichi <barbozasebastian408@gmail.com>
2026-01-02 18:06:36 -0400

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,18 +1,24 @@
-Let handler = async (m, { conn, isAdmin, isROwner }) => {
-    // Solo un administrador o el dueño del bot pueden 'enviar a Santa de vacaciones'
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    
-    // Prohíbe el chat: El bot se toma un descanso festivo.
-    global.db.data.chats[m.chat].isBanned = true
-    
-    // Mensaje festivo de desactivación
-    await conn.reply(m.chat, `✈️ ¡sᥲsᥙkᥱ ᑲ᥆𝗍 se fue de *Vacaciones Navideñas*! 🎁\n\nEl bot ha sido *DESACTIVADO* en este chat. ¡Volverá después de Reyes!`, m, rcanal)
-    
-    // Reacción que simboliza el descanso o un regalo
-    await m.react('😴') // O '🎁' si prefieres un regalo.
+let handler = async (m, { conn, isAdmin, isROwner }) => {
+  // Solo un administrador o el dueño del bot pueden 'enviar a Santa de vacaciones'
+  if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+
+  // Prohíbe el chat: El bot se toma un descanso festivo.
+  global.db.data.chats[m.chat].isBanned = true
+
+  // Mensaje festivo de desactivación
+  await conn.reply(
+    m.chat,
+    `✈️ ¡sᥲsᥙkᥱ ᑲ᥆𝗍 se fue de *Vacaciones Navideñas*! 🎁\n\nEl bot ha sido *DESACTIVADO* en este chat. ¡Volverá después de Reyes!`,
+    m
+  )
+
+  // Reacción que simboliza el descanso o un regalo
+  await m.react('😴') // O '🎁' si prefieres un regalo.
 }
+
 handler.help = ['banearbot']
 handler.tags = ['group']
 handler.command = ['banearbot', 'banchat']
-handler.group = true 
+handler.group = true
+
 export default handler
