5dd8cf40848ddcf56d63d6caa4c38a9ea0b692e4
Wichi <barbozasebastian408@gmail.com>
2026-01-08 21:39:34 -0400

    Actualizar girsl.js

--- plugins/girsl.js
+++ plugins/girsl.js
@@ -15,7 +15,7 @@ const fetchImage = async (url, timeout = 10000) => {
 };
 
 const imageCarouselHandler = async (m, { conn, command, usedPrefix, text = "girls" }) => {
-  const apiUrl = "https://delirius-apiofc.vercel.app/nsfw/girls";
+  const apiUrl = "https://api.delirius.store/nsfw/girls";
 
   try {
     const imagePromises = Array.from({ length: 6 }, () => fetchImage(apiUrl));
