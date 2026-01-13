f21448609b9f7c84c848fd4124544c89d4463e84
Wichi <barbozasebastian408@gmail.com>
2026-01-12 21:39:02 -0400

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
-let name = `CANAL NUEVO SIGAN`
+let name = `𝘾𝙖𝙣𝙖𝙡 𝙉𝙪𝙚𝙫𝙤 𝙂𝙚𝙣𝙩𝙚 𝙈𝙚 𝙎𝙞𝙜𝙪𝙚𝙣`
 /* let imagenes = ["https://raw.githubusercontent.com/WillZek/Storage-CB/main/images/c27c82d09da8.jpg",
 "https://raw.githubusercontent.com/WillZek/Storage-CB/main/images/c27c82d09da8.jpg",
 "https://raw.githubusercontent.com/WillZek/Storage-CB/main/images/c27c82d09da8.jpg",
@@ -19,7 +19,7 @@ global.rcanal = {
  contextInfo: {
              isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363414007802886@newsletter",
+      newsletterJid: "120363423619689248@newsletter",
       serverMessageId: 100,
       newsletterName: name,
    }, 
@@ -69,7 +69,7 @@ global.rcanal = {
     contextInfo: {
             isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363414007802886@newsletter",
+      newsletterJid: "120363423619689248@newsletter",
       serverMessageId: 100,
       newsletterName: name,
     },
