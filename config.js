5b53e92bf053d55723d80d1797d1aeb520605131
Wichi <barbozasebastian408@gmail.com>
2026-02-09 18:12:08 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ import axios from 'axios'
   global.owner = [
   [ '50671463198', 'eliud OFC', true ],
   [ '5804226480049', 'Ayudante', true ],
+  [ '573245743033', 'Ayudante', true ],
   ]
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
